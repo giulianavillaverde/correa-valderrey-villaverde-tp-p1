7dba7ba260713bc4d59ab53b35fa66aeff061f67
EduarVY <eduar.vr.e@gmail.com>
2026-05-17 21:37:42 -0300

    Proyecto base y fondo

---
+++ src/juego/Fondo.java
@@ -0,0 +1,24 @@
+package juego;
+
+import java.awt.Image;
+import entorno.Entorno;
+import entorno.Herramientas;
+
+public class Fondo {
+	double x, y, escala;
+	Image imagenFondo;
+	Entorno e;
+	
+	public Fondo(double x, double y, double escala, Entorno e) {
+		this.x = x;
+		this.y = y;
+		this.escala = escala;
+		this.imagenFondo = Herramientas.cargarImagen("fondo.jpg");
+		this.e = e;
+	}
+	
+	public void dibujar() {
+		e.dibujarImagen(imagenFondo, this.x, this.y, 0, this.escala);
+	}
+	
+}
